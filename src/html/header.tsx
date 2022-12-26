7d3df593bf1618b45a84053067c3a765c994c461
Muhssin Elnamli <ennemli.moh55@gmail.com>
2022-12-26 21:08:32 +0100

    warning

--- src/html/header.tsx
+++ src/html/header.tsx
@@ -1,7 +1,9 @@
 import githubLogo from '../images/logos/github-mark-white.svg'
 export default function Header(){
     return (<header>
+        <div className='warning'><p><span>Warning:</span> For a better experience please visit this page from computer.</p></div>
         <div className="repoLink">
+
             <a href='https://github.com/ennemli/retrowave' target={'_blink'}> <img src={githubLogo} alt="the main repository" /></a> 
         </div>
     </header>)
--- src/html/loading.tsx
+++ src/html/loading.tsx
@@ -3,11 +3,11 @@ import gifLoading from '../images/minto-dance.gif'
 export default function Loading({clicked,handleClick}:{clicked:boolean,handleClick:()=>void}){
     return (<>
     <div  className='loading'>
-    {clicked&&<div className='spinner'>
+    {!clicked&&<div className='spinner'>
       <a href="https://bit.ly/3jtvaSO" target="_blink"> <img src={gifLoading} alt="Loading"/></a>
       </div>
       }
-    {!clicked&&<button onClick={handleClick}>Begin Experience</button>}
+    {clicked&&<button onClick={handleClick}>Begin Experience</button>}
     
     </div>
     </>
